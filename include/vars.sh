7a673eb8fe15ec66f8c7c58fa977dc7d4a707bbd
Yuma Ohgami <yuma@ohgami.jp>
2022-09-23 22:07:38 +0900

    add cell data structure

--- include/vars.sh
+++ include/vars.sh
@@ -79,8 +79,18 @@ var_timer_handler=c026
 
 # バイナリ生物用変数
 var_cell_next_tile=c08a	# 次に描画する細胞タイルのタイル番号
-var_cell_tadr_bh=c08b	# 描画アドレス(下位8ビット)
-var_cell_tadr_th=c08c	# 描画アドレス(上位8ビット)
+var_cell_data_first_bh=c08b	# 1つ目の細胞データのアドレス(下位8ビット)
+var_cell_data_first_th=c08c	# 1つ目の細胞データのアドレス(上位8ビット)
+var_cell_data_current_bh=c08d	# 選択中の細胞データのアドレス(下位8ビット)
+var_cell_data_current_th=c08e	# 選択中の細胞データのアドレス(上位8ビット)
+
+# 0xc08f : 未使用
+# ※ 念の為、細胞データを2バイト境界に配置する為
+
+# 0xc090 - 0xc161 : 細胞データ1
+var_cell_tadr_bh=c092	# 描画アドレス(下位8ビット)
+var_cell_tadr_th=c093	# 描画アドレス(上位8ビット)
+# TODO 細胞データ初期化関数を追加し↑2つの変数は消す
 
 # 0xc300 - 0xcefd : TDQ
 # (include/tdq.sh 参照)
